3906ca3e7b3618695738134ac2f80244d0b1db7b
Jun <jun55tsuno@gmail.com>
2023-04-02 22:49:23 -0700

    updated

--- server/src/server.js
+++ server/src/server.js
@@ -8,7 +8,11 @@ const authRouters = require('./routes/authRote');
 
 const app = express();
 
-app.use(cors());
+app.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+	next();
+});
 app.use(express.json());
 
 app.use('/api/auth', authRouters);
